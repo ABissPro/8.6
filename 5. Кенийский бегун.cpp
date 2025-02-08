01ed8cacb1e235761c4cb92d8d3035420da99d7e
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-08 19:08:13 +0300

    Add files via upload

---
+++ 5. Кенийский бегун.cpp
@@ -0,0 +1,35 @@
+﻿#include <cmath>
+#include <iostream>
+
+int main()
+{
+    system("chcp 1251"); // Установить кодировку
+    int distance;
+    std::cout << "Привет, Сэм! Сколько километров ты сегодня пробежал? ";
+    std::cin >> distance;
+    int count;
+    int temp; //измеряется в секундах на километр по условию задачи
+    int sum = 0;
+
+    for (count = 1; count <= distance; count++) {
+        std::cout << "Какой у тебя был темп на километре " << count << "? ";
+        std::cin >> temp;
+        if (temp <= 0) {
+            std::cout << "Ошибка!!";
+        }
+        else {            
+            sum += temp;
+        }        
+    }
+
+    float average = (float)sum / (float)distance;
+    float min = average / 60;
+    min = std::floor(min);
+    int sec = (int)average % 60;
+
+
+    std::cout << "Твой средний темп за тренировку: " << min << " минут(ы) " << sec << " секунд(ы).";
+
+    
+}
+
